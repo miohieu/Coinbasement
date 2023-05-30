248919a2d0a970795db81f19ef68d57dd22487b3
hieuvu <hieu2108@gmail.com>
2023-05-30 17:13:53 +0700

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,22 @@
+import logo from "../public/logo.svg";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="container flex items-center">
+      <div className="navbar-left flex items-center">
+        <img src={logo} alt="" />
+        <h1 className="navbar-title ">Coinbasement</h1>
+      </div>
+      <div className="navbar-right flex ml-3 items-center">
+      <ul className="navbar-links flex list-none">
+        <li><a href="">Market</a></li>
+        <li><a href="">Explore</a></li>
+        <li><a href="">Portfolio</a></li>
+      </ul>
+      <div className="button-login">Login</div>
+      </div>
+      </div>
+    </nav>
+  );
+}
